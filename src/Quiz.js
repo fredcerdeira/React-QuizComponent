2f46295a69428e8413dfa3829a444b45eed2c7ed
Fred Cerdeira <frederico.cerdeira@sky.uk>
2018-07-18 10:18:13 +0100

    testing

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     }
 
     render() {
-        const isQuizEnd = false;
+        const isQuizEnd = true;
 
         if (isQuizEnd) {
             return <QuizEnd />
